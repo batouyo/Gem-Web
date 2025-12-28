4d91b3d3a06f3df2ce8a668832b1d46cd8305122
batouyo <1836760630@qq.com>
2025-12-29 03:23:57 +0800

    20251229

--- html/bib2json.py
+++ html/bib2json.py
@@ -2,12 +2,12 @@ import bibtexparser
 import json
 
 # 读取BibTeX文件
-with open('你的bib文件夾路徑', 'r', encoding='utf-8') as bib_file:
+with open('GEM_library.bib', 'r', encoding='utf-8') as bib_file:
     bib_database = bibtexparser.load(bib_file)
 
 # 将BibTeX对象转换为JSON格式
 json_data = json.dumps(bib_database.entries, indent=2)
 
 # 将JSON数据写入文件
-with open('output.json', 'w', encoding='utf-8') as json_file:
+with open('output2025.json', 'w', encoding='utf-8') as json_file:
     json_file.write(json_data)
